658a6755d20ba0870f1456897bb7fbea6977044a
marcSantolayaSanchez <marc.santolaya.7e7@itb.cat>
2024-05-02 10:01:11 +0200

    Hola

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,19 @@
+package com.example.plugins
+
+import com.example.models.Customer
+import com.example.routes.customerRouting
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+
+    routing {
+        customerRouting()
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+
+    val customerStorage = mutableListOf<Customer>()
+}
